a587145de361c6fd6f58e6f509ccf468a3a10bb1
missing <327770692@qq.com>
2018-09-05 10:27:28 +0800

    finished zb ok hb api

---
+++ python/w01_exchange/api/ok/ok_api.py
@@ -0,0 +1,140 @@
+
+
+
+
+import json, hashlib,struct,time,sys
+import urllib.request
+import urllib.parse as parse
+from pprint import pprint
+import sys
+
+import os
+CURRENTURL = os.path.dirname(__file__)
+sys.path.insert(1,CURRENTURL)
+# print(CURRENTURL)
+from OkcoinSpotAPI import OKCoinSpot
+
+class Ok_api:
+	
+    def __init__(self, mykey=None, mysecret=None):
+        if mykey and mysecret:
+            self.mykey,self.mysecret = mykey,mysecret
+        else:
+            filename = CURRENTURL+r'\key'
+            with open(filename,'r',encoding='utf-8') as f:
+                data = f.read()
+            self.mykey,self.mysecret = data.split('\n')
+            self.mykey = self.mykey.strip()
+            self.mysecret = self.mysecret.strip()
+
+        okcoinRESTURL = 'www.okb.com'
+        # print(self.mykey,self.mysecret)
+        self.okcoinSpot = OKCoinSpot(okcoinRESTURL,self.mykey,self.mysecret)
+
+    def __api_public_call(self,path,params=''):
+        headers = {
+        'user-agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36 Maxthon/5.1.3.2000',
+        }
+        try:
+            url = 'https://www.okb.com/api/v1/' + path + params
+            req = urllib.request.Request(url,headers=headers)
+            res = urllib.request.urlopen(req, timeout=10)
+            doc = json.loads(res.read().decode('utf-8')) # .decode('gbk', 'ignore')
+            return doc
+        except Exception as ex:
+            print(sys.stderr, 'ok request public_ex: ', ex)
+            return None
+
+            
+
+
+
+    def trades(self,symbol='btc_usdt',since=0):
+        '''
+            return (tid,symbol,date,price,amount,type)
+        '''
+
+        try:
+            data = self.okcoinSpot.trades(symbol,since)
+            lst = [da['tid'] for da in data]
+            return [(da['tid'],symbol,da['date'],da['price'],da['amount'],da['type']) for da in data]
+        except Exception as e:
+            # raise e
+            print('ok_api_ trades' ,e)
+            return []
+
+    def kline(self,symbol,type,size=500):
+        '''
+            symbol
+            type
+            size
+            since
+
+            https://www.okb.com/api/v1/kline.do
+        '''
+        try:
+            path = 'kline.do?'
+            params = "symbol=%s&type=%s&size=%s" %(symbol,type,size)
+            obj = self.__api_public_call(path, params)
+            # print (obj)
+            return obj
+        except Exception as ex:
+            print(sys.stderr, 'ok %s exception ,'%path,ex)
+            return None
+
+    def depth(self,symbol = ''):
+        data = self.okcoinSpot.depth(symbol)
+        return [data['asks'][::-1],data['bids']]
+
+    def order(self,symbol,price,amount,type):
+        data = self.okcoinSpot.trade(symbol,price,amount,type)
+        return data
+
+    def getOrder(self,id,symbol=None):
+        '''
+            {'orders': [{'amount': 1,
+                         'avg_price': 0,
+                         'create_date': 1536112711000,
+                         'deal_amount': 0,
+                         'order_id': 864345207,
+                         'orders_id': 864345207,
+                         'price': 10.0,
+                         'status': 0,
+                         'symbol': 'eos_usdt',
+                         'type': 'sell'}],
+             'result': True}
+        '''
+        data = self.okcoinSpot.orderinfo(symbol,id)
+        return data
+        
+    def unfinished_orders_list(self,symbol):
+        data = self.okcoinSpot.orderHistory(symbol)
+        return data
+
+
+    def cancelOrder(self,id,symbol):
+        '''
+            {'orders': [{'amount': 1,
+                         'avg_price': 0,
+                         'create_date': 1536112711000,
+                         'deal_amount': 0,
+                         'order_id': 864345207,
+                         'orders_id': 864345207,
+                         'price': 10.0,
+                         'status': 0,
+                         'symbol': 'eos_usdt',
+                         'type': 'sell'}],
+             'result': True}
+        '''
+        data = self.okcoinSpot.ordersinfo(symbol,id)
+        return data
+    # 
+    # 
+if __name__ == '__main__':
+    print(1)
+    api = ok_api()
+    data = api.trades('eos_btc',211342002)
+
+
+
+    print(list(data))
--- python/w01_exchange/run.py
+++ python/w01_exchange/run.py
@@ -6,14 +6,16 @@ sys.path.insert(1,CURRENTURL)
 
 from api.hb.hb_api import Hb_api
 from api.zb.zb_api import Zb_api
+from api.ok.ok_api import Ok_api
 from pprint import pprint
 
 
 
 def main():
-    # api = Zb_api()
-    # test(api)
-    api = Hb_api()
+    api = Ok_api()
+    test(api)
+    exit()
+    api = Zb_api()
     test(api)
 
 # 11629784439
@@ -22,10 +24,10 @@ def test(api):
     # data = api.kline('btc_usdt','1day',5) #dohlcv
     # data = api.depth('btc_usdt')
     # data = api.trades('btc_usdt');data = (len(data),data[0])
-    # data = api.order('bts_usdt',0.01,0.5,'buy')
-    # data = api.unfinished_orders_list('bts_usdt')
-    # data = api.getOrder(11629784439,'bts_usdt')
-    # data = api.cancelOrder('1162971184439','bts_usdt')
+    # data = api.order('eos_usdt',10,1,'sell')
+    # data = api.unfinished_orders_list('eos_usdt')
+    # data = api.getOrder(864345207,'eos_usdt')
+    # data = api.cancelOrder('864345207','eos_usdt')
     pprint(data)
 
 
